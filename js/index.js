bd4846c39192562b3ae3ccd2d9cdebe8880f85ca
SitongHui <492150523@qq.com>
2018-07-27 18:09:31 +0800

    修改

---
+++ js/index.js
@@ -0,0 +1,27 @@
+window.onload=function(){
+    tab();
+}
+
+function tab(){
+    //获取DOM元素
+    var titleEles = document.querySelectorAll(".tab > .tab-title > span");
+    var contentEles = document.querySelectorAll(".tab > .tab-content > li");
+
+    //注册事件
+    titleEles.forEach( (ele,index) => {
+        ele.addEventListener("click",(event) => {
+            clearEleStyle(titleEles,"selected");
+            clearEleStyle(contentEles,"selected");
+
+            event.target.classList.add("selected");
+            contentEles[index].classList.add("selected");
+        },false)
+    });
+
+    //工具函数
+    function clearEleStyle(obj,className){
+        obj.forEach( ele =>{
+            ele.classList.remove(className);
+        })
+    }
+}
